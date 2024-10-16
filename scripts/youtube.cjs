7b8f65ef677f74d37057e9d3a076a728ffa6e9b5
Alejandro <jose.alejandro.her.ros@gmail.com>
2024-10-16 07:03:45 +0200

    feature: post primeros 1k

--- scripts/youtube.cjs
+++ scripts/youtube.cjs
@@ -7,41 +7,67 @@ const API_KEY = process.env.API_KEY;
 const CHANNEL_ID = process.env.CHANNEL_ID;
 const youtube = google.youtube('v3');
 
-const params = {
-  key: API_KEY,
-  channelId: CHANNEL_ID,
-  order: 'date',
-  part: 'snippet',
-  maxResults: 5,
-  type: 'video', // Agregar el tipo de resultado como video
-  videoDuration: 'medium', // Duración del video: short, medium, long
-};
-
 const outputPath = path.join(__dirname, '..', 'src', 'config', 'youtube.json');
 
-youtube.search.list(params, (err, res) => {
-  if (err) {
-    console.error('Error al realizar la solicitud:', err);
-    return;
-  }
+const fetchVideos = async (duration) => {
+  const params = {
+    key: API_KEY,
+    channelId: CHANNEL_ID,
+    order: 'date',
+    part: 'snippet',
+    maxResults: 5,
+    type: 'video', // Add the type of result as video
+    videoDuration: duration, // Video duration: short, medium, long
+  };
 
-  const videos = res.data.items.filter((item) => item.id.kind === 'youtube#video');
-
-  const videoData = videos.map((video) => ({
-    title: video.snippet.title,
-    description: video.snippet.description,
-    channelId: video.snippet.channelId,
-    channelTitle: video.snippet.channelTitle,
-    videoId: video.id.videoId,
-    thumbnails: video.snippet.thumbnails,
-    publishedAt: video.snippet.publishedAt,
-  }));
-
-  fs.writeFile(outputPath, JSON.stringify(videoData, null, 2), (err) => {
-    if (err) {
-      console.error('Error al guardar los datos en el archivo:', err);
-    } else {
-      console.log('Los datos se han guardado en youtube.json');
-    }
+  return new Promise((resolve, reject) => {
+    youtube.search.list(params, (err, res) => {
+      if (err) {
+        return reject(err);
+      }
+      
+      const videos = res.data.items.filter((item) => item.id.kind === 'youtube#video');
+      const videoData = videos.map((video) => ({
+        title: video.snippet.title,
+        description: video.snippet.description,
+        channelId: video.snippet.channelId,
+        channelTitle: video.snippet.channelTitle,
+        videoId: video.id.videoId,
+        thumbnails: video.snippet.thumbnails,
+        publishedAt: video.snippet.publishedAt,
+      }));
+      
+      resolve(videoData);
+    });
   });
-});
+};
+
+const main = async () => {
+  try {
+    // Fetch medium duration videos
+    const mediumVideos = await fetchVideos('medium');
+    
+    // Fetch long duration videos
+    const longVideos = await fetchVideos('long');
+    
+    // Combine results
+    const allVideos = [...mediumVideos, ...longVideos];
+
+    // Sort videos by published date (publishedAt)
+    allVideos.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
+
+    // Save to file
+    fs.writeFile(outputPath, JSON.stringify(allVideos, null, 2), (err) => {
+      if (err) {
+        console.error('Error saving data to the file:', err);
+      } else {
+        console.log('Data has been saved to youtube.json');
+      }
+    });
+    
+  } catch (error) {
+    console.error('Error making the request:', error);
+  }
+};
+
+main();
---
+++ src/content/blog/como-invertir-tu-primeros-1000.md
@@ -0,0 +1,29 @@
+---
+title: "Cómo Invertir tus Primeros 1,000 Euros o Dólares de Forma Inteligente"
+description: "Descubre cómo invertir tus primeros 1,000 euros o dólares de forma inteligente. En este post, te contamos cómo evaluar tu situación financiera, considerar si esta cantidad es suficiente para comenzar, y por qué podrías priorizar invertir en ti mismo."
+pubDate: "2024-10-16T04:35:27.866Z"
+heroImage: "/blogs/1k.png"
+categories: ["Inversiones para Principiantes", "Consejos Financieros", "Blog Inversiones"] 
+tags: ["Inversiones Iniciales", "Cómo Invertir", "Primeros Pasos Financieros", "Invertir con Poco Dinero", "Libertad Financiera", "Inversión Inteligente", "Maximizar Ganancias", "Principiantes en Inversiones", "Invertir 1000 Euros", "Consejos para Invertir"]
+author: ["Alejandro Rosales"]
+---
+
+Ey! Hoy te traigo una guía rápida sobre cómo invertir tus primeros 1,000 euros o dólares de manera inteligente. ¿Listo para descubrir las mejores opciones? ¡Vamos a ello!
+
+## Paso 1: Evalúa Tu Situación Financiera
+Antes de lanzarte a invertir, es fundamental entender en qué situación financiera te encuentras. ¿Es esta tu primera inversión? Dependiendo de tu punto de partida, la estrategia puede variar. No todos los consejos funcionan para todos.
+
+## Paso 2: ¿Vale la Pena Invertir si Solo Tienes 1,000 al Año?
+Puede que pienses que invertir 1,000 euros o dólares anuales es suficiente, pero ¿sabes cuánto podrías ganar realmente? En el video, te cuento si esta cantidad es rentable o si es mejor invertirla en otra cosa... ¡como en ti mismo! Así podrías incrementar tu capacidad de ahorro y obtener mucho más a futuro.
+
+## Paso 3: Considera Aumentar Tu Valor Personal
+A veces, la mejor inversión que puedes hacer es en ti mismo. Un aumento en tus habilidades podría llevarte a ganar más y ahorrar más cada año. Si te interesa saber cómo esto puede transformar tus finanzas, ¡no te lo pierdas!
+
+Te cuento todo esto y más en el video de YouTube, donde desgloso las opciones más inteligentes y cómo decidir cuál es la adecuada para ti. Haz clic aquí para verlo ahora.
+
+
+¿Listo para profundizar? No te pierdas el video completo en YouTube. Haz clic en el enlace y descubre cómo invertir tus primeros 1,000 de manera estratégica. ¡Nos vemos en el video!
+
+<div class="iframe-container" style="position: relative; width: 100%; height: 0; padding-bottom: 56.25%; overflow: hidden;">
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/lIpdthTbC3Y?si=Hp-qYyfFL4JFvT67" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none;"></iframe>
+</div>
